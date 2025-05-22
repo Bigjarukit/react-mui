dae4b88d34540eaeae29e0bbf87aa957c63f5621
jarukit <jarukitatwork@gmail.com>
2025-05-22 09:23:12 +0700

    [add]: //

--- src/page/LoginPage/LoginPageTest.tsx
+++ src/page/LoginPage/LoginPageTest.tsx
@@ -15,6 +15,7 @@ export default function LoginPageTest() {
   const fnDelete = (index: number) => {
     const copy = [...arrays];
     copy.splice(index, 1);
+      // 
     setArrays(copy);
   };
 
